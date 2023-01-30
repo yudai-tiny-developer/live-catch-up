9ef2769accee0303505f4d2efdd312e967443b65
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2023-01-30 20:33:55 +0900

    remove listener

--- content.js
+++ content.js
@@ -1,41 +1,38 @@
 import(chrome.runtime.getURL('common.js')).then(common => {
-	let button;
-	let playbackRate = common.defaultPlaybackRate;
+    let playbackRate = common.defaultPlaybackRate;
 
-	chrome.storage.local.get(['playbackRate'], (data) => {
-		playbackRate = common.limitPlaybackRate(data.playbackRate);
-	});
+    chrome.storage.local.get(['playbackRate'], (data) => {
+        playbackRate = common.limitPlaybackRate(data.playbackRate);
+    });
 
-	chrome.storage.onChanged.addListener((changes, namespace) => {
-		chrome.storage.local.get(['playbackRate'], (data) => {
-			playbackRate = common.limitPlaybackRate(data.playbackRate);
-		});
-	});
+    chrome.storage.onChanged.addListener((changes, namespace) => {
+        chrome.storage.local.get(['playbackRate'], (data) => {
+            playbackRate = common.limitPlaybackRate(data.playbackRate);
+        });
+    });
 
-	const app = document.querySelector('ytd-app');
-	if (app) {
-		new MutationObserver((mutations, observer) => {
-			for (const m of mutations) {
-				if (m.target.nodeName === 'BUTTON' && m.target.classList.contains('ytp-live-badge') && m.target.classList.contains('ytp-button')) {
-					if (button !== m.target) {
-						button = m.target;
-						const video = app.querySelector('video');
-						video.addEventListener('progress', () => {
-							if (button.hasAttribute('disabled')) {
-								video.playbackRate = 1.0;
-							} else {
-								video.playbackRate = playbackRate;
-							}
-						});
-					}
-					return;
-				}
-			}
-		}).observe(app, {
-			subtree: true,
-			childList: true,
-		});
-	} else {
-		console.warn('ytd-app not found');
-	}
+    const app = document.querySelector('ytd-app');
+    if (app) {
+        new MutationObserver((mutations, observer) => {
+            for (const m of mutations) {
+                const target = m.target;
+                if (target.nodeName === 'BUTTON' && target.classList.contains('ytp-live-badge') && target.classList.contains('ytp-button')) {
+                    const video = app.querySelector('video');
+                    if (video) {
+                        if (target.hasAttribute('disabled')) {
+                            video.playbackRate = 1.0;
+                        } else {
+                            video.playbackRate = playbackRate;
+                        }
+                        return;
+                    }
+                }
+            }
+        }).observe(app, {
+            subtree: true,
+            childList: true,
+        });
+    } else {
+        console.warn('ytd-app not found');
+    }
 });
