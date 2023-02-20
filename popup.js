cb6e534133a88499d37c81ab7c24cc1e0556ad72
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2023-02-21 04:41:32 +0900

    v1.1.3

--- popup.js
+++ popup.js
@@ -1,20 +1,19 @@
 import(chrome.runtime.getURL('common.js')).then(common => {
-    function createLabel(data) {
+    function createLabel() {
         const div = document.createElement('div');
         div.id = 'label';
-        div.type = 'number';
         div.innerHTML = `Playback Rate (${common.minPlaybackRate.toFixed(2)} ~ ${common.maxPlaybackRate.toFixed(2)})`;
         return div;
     }
 
-    function createInput(data) {
+    function createInput(playbackRate) {
         const input = document.createElement('input');
         input.id = 'playbackRate';
         input.type = 'number';
         input.min = common.minPlaybackRate;
         input.max = common.maxPlaybackRate;
         input.step = common.stepPlaybackRate;
-        input.value = common.limitPlaybackRate(data.playbackRate);
+        input.value = common.limitPlaybackRate(playbackRate);
         input.addEventListener('change', () => {
             chrome.storage.local.set({ 'playbackRate': common.limitPlaybackRate(input.value) });
         });
@@ -23,7 +22,7 @@ import(chrome.runtime.getURL('common.js')).then(common => {
 
     chrome.storage.local.get(['playbackRate'], (data) => {
         const div = document.querySelector('div#container');
-        div.appendChild(createLabel(data));
-        div.appendChild(createInput(data));
+        div.appendChild(createLabel());
+        div.appendChild(createInput(data.playbackRate));
     });
 });
