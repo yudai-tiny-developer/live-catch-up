2215bf97caf4f40c5967a3656cce09130ec917a6
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2023-12-17 14:00:33 +0900

    ;

--- popup.js
+++ popup.js
@@ -69,7 +69,7 @@ import(chrome.runtime.getURL('common.js')).then(common => {
         input.max = maxRate;
         input.step = stepRate;
         input.addEventListener('change', () => {
-            chrome.storage.local.set({ [key]: common.limitRate(input.value, defaultValue, minRate, maxRate, stepRate); });
+            chrome.storage.local.set({ [key]: common.limitRate(input.value, defaultValue, minRate, maxRate, stepRate) });
         });
         cell.appendChild(input);
 
