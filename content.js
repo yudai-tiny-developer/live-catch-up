b1e18626fdeb73c0c96f5df69fd2fcbc378ba14d
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2024-02-04 12:02:50 +0900

    V1.5.0 (#13)
    * github actions
    
    * support holodex.net
    
    ---------
    
    Co-authored-by: yudai-tiny-developer <yudai-tiny-developer@gmail.com>

--- content.js
+++ content.js
@@ -1,51 +1,48 @@
 import(chrome.runtime.getURL('common.js')).then(common => {
-    const app = document.querySelector('ytd-app');
-    if (app) {
-        let enabled = common.defaultEnabled;
-        let playbackRate = common.defaultPlaybackRate;
+    let enabled = common.defaultEnabled;
+    let playbackRate = common.defaultPlaybackRate;
 
-        function initSettings() {
-            chrome.storage.local.get(common.storage, data => {
-                enabled = common.value(data.enabled, common.defaultEnabled);
-                playbackRate = common.limitRate(data.playbackRate, common.defaultPlaybackRate, common.minPlaybackRate, common.maxPlaybackRate, common.stepPlaybackRate);
+    function initSettings() {
+        chrome.storage.local.get(common.storage, data => {
+            enabled = common.value(data.enabled, common.defaultEnabled);
+            playbackRate = common.limitRate(data.playbackRate, common.defaultPlaybackRate, common.minPlaybackRate, common.maxPlaybackRate, common.stepPlaybackRate);
 
-                changePlaybackRate(app.querySelector('.ytp-live-badge'));
-            });
-        }
-
-        initSettings();
-
-        chrome.storage.onChanged.addListener(() => {
-            initSettings();
-        });
-
-        function setPlaybackRate(playbackRate) {
-            for (const media of document.querySelectorAll('video')) {
-                media.playbackRate = playbackRate;
-            }
-        }
-
-        function changePlaybackRate(badge) {
-            if (enabled) {
-                if (badge) {
-                    setPlaybackRate(badge.hasAttribute('disabled') ? 1.0 : playbackRate);
-                } else {
-                    // do nothing
-                }
-            } else {
-                setPlaybackRate(1.0);
-            }
-        }
-
-        new MutationObserver(mutations => {
-            mutations.filter(m => {
-                return m.target.classList.contains('ytp-live-badge');
-            }).forEach(m => {
-                changePlaybackRate(m.target);
-            });
-        }).observe(app, {
-            attributeFilter: ['disabled'],
-            subtree: true,
+            changePlaybackRate(document.querySelector('.ytp-live-badge'));
         });
     }
+
+    initSettings();
+
+    chrome.storage.onChanged.addListener(() => {
+        initSettings();
+    });
+
+    function setPlaybackRate(playbackRate) {
+        for (const media of document.querySelectorAll('video')) {
+            media.playbackRate = playbackRate;
+        }
+    }
+
+    function changePlaybackRate(badge) {
+        if (enabled) {
+            if (badge) {
+                setPlaybackRate(badge.hasAttribute('disabled') ? 1.0 : playbackRate);
+            } else {
+                // do nothing
+            }
+        } else {
+            setPlaybackRate(1.0);
+        }
+    }
+
+    new MutationObserver(mutations => {
+        mutations.filter(m => {
+            return m.target.classList.contains('ytp-live-badge');
+        }).forEach(m => {
+            changePlaybackRate(m.target);
+        });
+    }).observe(document, {
+        attributeFilter: ['disabled'],
+        subtree: true,
+    });
 });
