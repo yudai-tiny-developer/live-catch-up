84322be4494b2ddc29290c997fd2a106cfbd8d5f
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2024-06-17 01:38:45 +0900

    V1.9.0 (#28)
    * v1.9.0
    
    * v1.9.0
    
    ---------
    
    Co-authored-by: yudai-tiny-developer <yudai-tiny-developer@gmail.com>

--- inject.js
+++ inject.js
@@ -1,19 +1,43 @@
 let _live_catch_up_interval;
+let _live_catch_up_app = document.body.querySelector('ytd-app');
 let _live_catch_up_player;
 let _live_catch_up_media;
+
+function calcPlaybackRate(isAtLiveHead, stats_lat, playbackRate, smoothThreathold, slowdownAtLiveHead) {
+    if (isAtLiveHead) {
+        if (stats_lat < smoothThreathold) {
+            return 1.0;
+        } else if (slowdownAtLiveHead) {
+            return 1.05;
+        } else {
+            return playbackRate;
+        }
+    } else {
+        return playbackRate;
+    }
+
+}
 
 document.addEventListener('_live_catch_up_start', e => {
     const playbackRate = e.detail.playbackRate;
     const smoothRate = e.detail.smoothRate;
     const smoothThreathold = e.detail.smoothThreathold;
+    const slowdownAtLiveHead = e.detail.slowdownAtLiveHead;
 
     clearInterval(_live_catch_up_interval);
 
     _live_catch_up_interval = setInterval(() => {
+        if (!_live_catch_up_app) {
+            _live_catch_up_app = document.body.querySelector('ytd-app');
+            if (!_live_catch_up_app) {
+                return;
+            }
+        }
+
         if (!_live_catch_up_media || !_live_catch_up_player || !_live_catch_up_player.getVideoStats || !_live_catch_up_player.isAtLiveHead) {
-            _live_catch_up_media = undefined;
-            _live_catch_up_player = document.querySelector('div#movie_player');
+            _live_catch_up_player = _live_catch_up_app.querySelector('div#movie_player');
             if (!_live_catch_up_player || !_live_catch_up_player.getVideoStats || !_live_catch_up_player.isAtLiveHead) {
+                _live_catch_up_media = undefined;
                 return;
             }
 
@@ -25,7 +49,7 @@ document.addEventListener('_live_catch_up_start', e => {
 
         const stats = _live_catch_up_player.getVideoStats();
         if (stats && stats.live) {
-            const newPlaybackRate = _live_catch_up_player.isAtLiveHead() && stats.lat < smoothThreathold ? 1.0 : playbackRate;
+            const newPlaybackRate = calcPlaybackRate(_live_catch_up_player.isAtLiveHead(), stats.lat, playbackRate, smoothThreathold, slowdownAtLiveHead);
             if (_live_catch_up_media.playbackRate !== newPlaybackRate) {
                 _live_catch_up_media.playbackRate = newPlaybackRate;
             }
