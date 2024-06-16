4b515983556776d0e3ab3f0f96759e03083f0a3b
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2024-06-16 15:51:08 +0900

    V1.8.7 (#26)
    * v1.8.7
    
    * performance tuning
    
    * performance tuning
    
    ---------
    
    Co-authored-by: yudai-tiny-developer <yudai-tiny-developer@gmail.com>

--- inject.js
+++ inject.js
@@ -1,4 +1,6 @@
 let _live_catch_up_interval;
+let _live_catch_up_player;
+let _live_catch_up_media;
 
 document.addEventListener('_live_catch_up_start', e => {
     const playbackRate = e.detail.playbackRate;
@@ -8,20 +10,25 @@ document.addEventListener('_live_catch_up_start', e => {
     clearInterval(_live_catch_up_interval);
 
     _live_catch_up_interval = setInterval(() => {
-        const player = document.querySelector('div#movie_player');
-        if (!player || !player.getVideoStats || !player.isAtLiveHead) {
-            return;
+        if (!_live_catch_up_media || !_live_catch_up_player || !_live_catch_up_player.getVideoStats || !_live_catch_up_player.isAtLiveHead) {
+            _live_catch_up_media = undefined;
+            _live_catch_up_player = document.querySelector('div#movie_player');
+            if (!_live_catch_up_player || !_live_catch_up_player.getVideoStats || !_live_catch_up_player.isAtLiveHead) {
+                return;
+            }
+
+            _live_catch_up_media = _live_catch_up_player.querySelector('video.video-stream');
+            if (!_live_catch_up_media) {
+                return;
+            }
         }
 
-        const media = player.querySelector('video');
-        if (!media) {
-            return;
-        }
-
-        const stats = player.getVideoStats();
-        if (stats.live) {
-            media.playbackRate = player.isAtLiveHead() && stats.lat < smoothThreathold ? 1.0 : playbackRate;
-            media.classList.add('_live_catch_up');
+        const stats = _live_catch_up_player.getVideoStats();
+        if (stats && stats.live) {
+            const newPlaybackRate = _live_catch_up_player.isAtLiveHead() && stats.lat < smoothThreathold ? 1.0 : playbackRate;
+            if (_live_catch_up_media.playbackRate !== newPlaybackRate) {
+                _live_catch_up_media.playbackRate = newPlaybackRate;
+            }
         }
     }, smoothRate);
 });
