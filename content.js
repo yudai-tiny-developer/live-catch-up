1b51dc786b20b153236298b1196535cb30ded20e
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2025-02-28 08:07:58 +0900

    V1.19.1 (#68)
    * v1.19.1
    
    * \n
    
    * loadSettings
    
    * if
    
    * detect_interval
    
    * detect_interval
    
    ---------
    
    Co-authored-by: yudai-tiny-developer <yudai-tiny-developer@gmail.com>

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ function main(common) {
     function loadSettings() {
         chrome.storage.local.get(common.storage, data => {
             const enabled = common.value(data.enabled, common.defaultEnabled);
-            const playbackRate = common.limitValue(data.playbackRate, common.defaultPlaybackRate, common.minPlaybackRate, common.maxPlaybackRate, common.stepPlaybackRate);
+            playbackRate = common.limitValue(data.playbackRate, common.defaultPlaybackRate, common.minPlaybackRate, common.maxPlaybackRate, common.stepPlaybackRate);
             const showPlaybackRate = common.value(data.showPlaybackRate, common.defaultShowPlaybackRate);
             const showLatency = common.value(data.showLatency, common.defaultShowLatency);
             const showHealth = common.value(data.showHealth, common.defaultShowHealth);
@@ -18,15 +18,8 @@ function main(common) {
 
             sendLoadSettingsEvent(enabled, playbackRate, showPlaybackRate, showLatency, showHealth, showEstimation, smooth, smoothThreathold);
 
-            badge_observer?.disconnect();
-            container_observer?.disconnect();
-
             if (enabled) {
-                if (smooth) {
-                    setPlaybackRate(playbackRate);
-                } else {
-                    container_observer = is_embedded_player ? observe_player(document, playbackRate) : observe_app(document, playbackRate);
-                }
+                setPlaybackRate(playbackRate);
             } else {
                 setPlaybackRate();
             }
@@ -85,76 +78,33 @@ function main(common) {
         document.dispatchEvent(new CustomEvent('_live_catch_up_reset_playback_rate'));
     }
 
-    function observe(node, query, callback, param) {
-        const target = node.querySelector(query);
-        if (target) {
-            return callback(target, param);
-        } else {
-            const observer = new MutationObserver((mutations, observer) => {
-                const target = node.querySelector(query);
-                if (target && callback(target, param)) {
-                    observer.disconnect();
-                }
-            });
-            observer.observe(node, { childList: true, subtree: true });
-            return observer;
-        }
-    }
-
-    function observe_app(node, param) {
-        return observe(node, 'ytd-app', observe_player, param);
-    }
-
-    function observe_player(node, param) {
-        return observe(node, 'div#movie_player', observe_main, param);
-    }
-
-    function observe_main(node, param) {
-        badge = node.querySelector('button.ytp-live-badge');
-        if (badge) {
-            setPlaybackRate(param);
-            badge_observer = new MutationObserver(() => {
-                setPlaybackRate(param);
-            });
-            badge_observer.observe(badge, { attributeFilter: ['disabled'] })
-            return badge_observer;
-        }
-    }
-
-    function inject() {
-        const s = document.createElement('script');
-        s.id = '_live_catch_up';
-        s.src = chrome.runtime.getURL('inject.js');
-        s.onload = () => s.remove();
-        (document.head || document.documentElement).append(s);
-    }
+    const app = document.querySelector('ytd-app') ?? document.body; // YouTube.com or Embedded Player
 
     let badge;
-    let container_observer;
-    let badge_observer;
-    let is_embedded_player;
+    let playbackRate;
 
     chrome.storage.onChanged.addListener(loadSettings);
 
-    document.addEventListener('_live_catch_up_init', loadSettings);
-
-    const init_interval = setInterval(() => {
-        if (document.readyState === 'complete') {
-            const app = document.querySelector('ytd-app');
-            if (app) { // YouTube.com Player
-                clearInterval(init_interval);
-                is_embedded_player = false;
-                inject();
+    document.addEventListener('_live_catch_up_init', () => {
+        const detect_interval = setInterval(() => {
+            badge = app.querySelector('button.ytp-live-badge');
+            if (!badge) {
                 return;
             }
 
-            const player = document.querySelector('div#movie_player');
-            if (player) { // Embedded Player
-                clearInterval(init_interval);
-                is_embedded_player = true;
-                inject();
-                return;
-            }
-        }
-    }, 200);
+            clearInterval(detect_interval);
+
+            new MutationObserver(() => {
+                setPlaybackRate(playbackRate);
+            }).observe(badge, { attributeFilter: ['disabled'] });
+
+            loadSettings();
+        }, 500);
+    });
+
+    const s = document.createElement('script');
+    s.id = '_live_catch_up';
+    s.src = chrome.runtime.getURL('inject.js');
+    s.onload = () => s.remove();
+    (document.head || document.documentElement).append(s);
 }
