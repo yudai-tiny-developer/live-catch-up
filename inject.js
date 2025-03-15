45c6d273f0e11bd4f12478c3d058b8bb22e0260e
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2025-03-15 21:43:36 +0900

    V1.20.2 (#76)
    * v1.20.2
    
    * reinit
    
    ---------
    
    Co-authored-by: yudai-tiny-developer <yudai-tiny-developer@gmail.com>

--- inject.js
+++ inject.js
@@ -115,6 +115,10 @@
         }
     }
 
+    function onPlaybackRateChange() {
+        document.dispatchEvent(new CustomEvent('_live_catch_up_onPlaybackRateChange'));
+    }
+
     const HTMLPolicy = window.trustedTypes ? window.trustedTypes.createPolicy("_live_catch_up_HTMLPolicy", { createHTML: (string) => string }) : { createHTML: (string) => string };
 
     const button_playbackrate = document.createElement('button');
@@ -221,6 +225,8 @@
         }
 
         clearInterval(detect_interval);
+
+        player.addEventListener('onPlaybackRateChange', onPlaybackRateChange);
 
         badge.parentElement.parentElement.appendChild(button_estimation);
         badge.parentElement.parentElement.insertBefore(button_health, button_estimation);
