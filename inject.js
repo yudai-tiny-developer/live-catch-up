37c590c0f78daaec1d36634ec030fbbe56ec839a
Simple and Tiny Utilities <yudai-tiny-developer@gmail.com>
2025-07-28 09:12:38 +0900

    style

--- inject.js
+++ inject.js
@@ -2,21 +2,15 @@
     function update_playbackRate(playbackRate) {
         const video = video_instance();
         if (video) {
-            if (new_style) {
-                button_playbackrate.innerHTML = HTMLPolicy.createHTML(`<svg width="90%" height="90%" viewBox="0 0 72 72"><text font-size="20" x="0%" y="35%">${video.playbackRate.toFixed(2)}x</text></svg>`);
-            } else {
-                button_playbackrate.innerHTML = HTMLPolicy.createHTML(`<svg width="100%" height="100%" viewBox="0 0 72 72"><text font-size="20" x="50%" y="50%" dominant-baseline="central" text-anchor="middle">${video.playbackRate.toFixed(2)}x</text></svg>`);
-            }
+            button_playbackrate.innerHTML = HTMLPolicy.createHTML(`<span class="ytp-live">${video.playbackRate.toFixed(2)}x</span>`);
 
             if (video.playbackRate === playbackRate) {
-                button_playbackrate.style.fill = '#ff8983';
-                button_playbackrate.style.fontWeight = 'bold';
+                button_playbackrate.style.color = '#ff8983';
             } else {
-                button_playbackrate.style.fill = '#eee';
-                button_playbackrate.style.fontWeight = 'normal';
+                button_playbackrate.style.color = '#eee';
             }
 
-            button_playbackrate.style.display = '';
+            button_playbackrate.style.display = 'inline-block';
         } else {
             button_playbackrate.style.display = 'none';
         }
@@ -28,22 +22,12 @@
 
     function update_latency(latency, isAtLiveHead) {
         if (isAtLiveHead) {
-            if (new_style) {
-                button_latency.innerHTML = HTMLPolicy.createHTML(`<svg width="90%" height="90%" viewBox="0 0 72 72"><text font-size="20" x="0%" y="35%">${latency.toFixed(2)}s</text></svg>`);
-            } else {
-                button_latency.innerHTML = HTMLPolicy.createHTML(`<svg width="100%" height="100%" viewBox="0 0 72 72"><text font-size="20" x="50%" y="50%" dominant-baseline="central" text-anchor="middle">${latency.toFixed(2)}s</text></svg>`);
-            }
+            button_latency.innerHTML = HTMLPolicy.createHTML(`<span class="ytp-live">${latency.toFixed(2)}s</span>`);
         } else {
-            if (new_style) {
-                button_latency.innerHTML = HTMLPolicy.createHTML(`<svg width="90%" height="90%" viewBox="0 0 72 72"><text font-size="20" x="0%" y="35%">(DVR)</text></svg>`);
-            } else {
-                button_latency.innerHTML = HTMLPolicy.createHTML(`<svg width="100%" height="100%" viewBox="0 0 72 72"><text font-size="20" x="50%" y="50%" dominant-baseline="central" text-anchor="middle">(DVR)</text></svg>`);
-            }
+            button_latency.innerHTML = HTMLPolicy.createHTML(`<span class="ytp-live">(DVR)</span>`);
         }
 
-        button_latency.style.fill = '#eee';
-        button_latency.style.fontWeight = 'normal';
-        button_latency.style.display = '';
+        button_latency.style.display = 'inline-block';
     }
 
     function hide_latency() {
@@ -51,21 +35,15 @@
     }
 
     function update_health(health, enabled, smoothThreathold) {
-        if (new_style) {
-            button_health.innerHTML = HTMLPolicy.createHTML(`<svg width="90%" height="90%" viewBox="0 0 72 72"><text font-size="20" x="0%" y="35%">${health.toFixed(2)}s</text></svg>`);
-        } else {
-            button_health.innerHTML = HTMLPolicy.createHTML(`<svg width="100%" height="100%" viewBox="0 0 72 72"><text font-size="20" x="50%" y="50%" dominant-baseline="central" text-anchor="middle">${health.toFixed(2)}s</text></svg>`);
-        }
+        button_health.innerHTML = HTMLPolicy.createHTML(`<span class="ytp-live">${health.toFixed(2)}s</span>`);
 
         if (enabled && health >= smoothThreathold) {
-            button_health.style.fill = '#ff8983';
-            button_health.style.fontWeight = 'bold';
+            button_health.style.color = '#ff8983';
         } else {
-            button_health.style.fill = '#eee';
-            button_health.style.fontWeight = 'normal';
+            button_health.style.color = '#eee';
         }
 
-        button_health.style.display = '';
+        button_health.style.display = 'inline-block';
     }
 
     function hide_health() {
@@ -79,15 +57,8 @@
         const estimated_seconds = (seekableEnd - current) / (streamHasProbablyEnded ? video.playbackRate : video.playbackRate - 1.0);
         if (!isAtLiveHead && isFinite(estimated_seconds)) {
             const estimated_time = new Date(Date.now() + estimated_seconds * 1000.0).toLocaleTimeString();
-            const length = String(estimated_time).length;
-
-            if (new_style) {
-                button_estimation.innerHTML = HTMLPolicy.createHTML(`<svg width="90%" height="90%" viewBox="0 0 ${length * 12} 72"><text font-size="20" x="0%" y="35%">${estimated_time}</text></svg>`);
-            } else {
-                button_estimation.innerHTML = HTMLPolicy.createHTML(`<svg width="100%" height="100%" viewBox="0 0 ${length * 12} 72"><text font-size="20" x="50%" y="50%" dominant-baseline="central" text-anchor="middle">${estimated_time}</text></svg>`);
-            }
-
-            button_estimation.style.display = '';
+            button_estimation.innerHTML = HTMLPolicy.createHTML(`<span class="ytp-live">(${estimated_time})</span>`);
+            button_estimation.style.display = 'inline-block';
         } else {
             button_estimation.style.display = 'none';
         }
@@ -101,26 +72,16 @@
         const current_time = format_time(current);
 
         if (isAtLiveHead) {
-            const length = String(current_time).length;
-            if (new_style) {
-                button_current.innerHTML = HTMLPolicy.createHTML(`<svg width="90%" height="90%" viewBox="0 0 ${length * 12} 72"><text font-size="20" x="0%" y="35%">${current_time}</text></svg>`);
-            } else {
-                button_current.innerHTML = HTMLPolicy.createHTML(`<svg width="100%" height="100%" viewBox="0 0 ${length * 12} 72"><text font-size="20" x="50%" y="50%" dominant-baseline="central" text-anchor="middle">${current_time}</text></svg>`);
-            }
+            button_current.innerHTML = HTMLPolicy.createHTML(`<span class="ytp-live">${current_time}</span>`);
         } else {
             const seekableEnd_time = format_time(seekableEnd);
-            const length = String(current_time).length + String(seekableEnd_time).length;
-            if (new_style) {
-                button_current.innerHTML = HTMLPolicy.createHTML(`<svg width="90%" height="90%" viewBox="0 0 ${length * 12} 72"><text font-size="20" x="0%" y="35%">${current_time} / ${seekableEnd_time}</text></svg>`);
-            } else {
-                button_current.innerHTML = HTMLPolicy.createHTML(`<svg width="100%" height="100%" viewBox="0 0 ${length * 12} 72"><text font-size="20" x="50%" y="50%" dominant-baseline="central" text-anchor="middle">${current_time} / ${seekableEnd_time}</text></svg>`);
-            }
+            button_current.innerHTML = HTMLPolicy.createHTML(`<span class="ytp-live">${current_time} / ${seekableEnd_time}</span>`);
         }
 
         const current_time_url = addParamsToUrl('https://www.youtube.com/watch', { v: videoId, t: format_time_hms(current) });
         button_current.setAttribute('current', `${current_time_url}#\n${current_time}`);
 
-        button_current.style.display = '';
+        button_current.style.display = 'inline-block';
     }
 
     function hide_current() {
@@ -255,9 +216,12 @@
     }
 
     function bonus_features() {
-        const time_display = player.querySelector('div.ytp-time-display');
+        const time_display = player.querySelector('div.ytp-time-contents');
         if (time_display && !time_display.hasAttribute('_live_catch_up_bonus_features')) {
-            time_display.addEventListener('click', () => {
+            time_display.addEventListener('click', e => {
+                e.stopPropagation();
+                e.preventDefault();
+
                 if (showCurrent && !time_display.classList.contains('ytp-live')) {
                     const current = player.getProgressState()?.current;
                     const videoId = player.getVideoData()?.video_id;
@@ -267,82 +231,79 @@
 
                     navigator.clipboard.writeText(`${current_time_url}#\n${current_time}`);
 
-                    const rect = time_display.getBoundingClientRect();
-                    msg_current.style.left = `${rect.left + rect.width / 2.0}px`;
-                    msg_current.style.top = `${rect.top - 16}px`;
-                    msg_current.style.display = 'block';
-                    const timeout_id = setTimeout(() => {
-                        if (msg_current_timeout === timeout_id) {
-                            msg_current.style.display = 'none';
-                        }
+                    if (new_style) {
+                        msg_current.style.translate = '-32px -16px';
+                    } else {
+                        const rect = button_current.getBoundingClientRect();
+                        msg_current.style.left = `${rect.left + rect.width / 2.0}px`;
+                        msg_current.style.top = `${rect.top - 16}px`;
+                    }
+
+                    msg_current.style.display = 'inline-block';
+
+                    clearTimeout(msg_current_timeout);
+                    msg_current_timeout = setTimeout(() => {
+                        msg_current.style.display = 'none';
                     }, 4000);
-                    msg_current_timeout = timeout_id;
                 }
             });
             time_display.setAttribute('_live_catch_up_bonus_features', '');
         }
     }
 
+    function create_elem(elem_name, elem_classes) {
+        const elem = document.createElement(elem_name);
+        elem.classList.add(...elem_classes);
+        elem.style.display = 'none';
+        elem.style.cursor = 'default';
+        elem.style.textAlign = 'center';
+        elem.style.width = 'auto';
+        elem.style.height = 'auto';
+        elem.style.color = '#eee';
+        elem.style.fontWeight = 'normal';
+        elem.style.paddingLeft = '8px';
+        elem.style.paddingRight = '8px';
+        return elem;
+    }
+
     const HTMLPolicy = window.trustedTypes ? window.trustedTypes.createPolicy("_live_catch_up_HTMLPolicy", { createHTML: (string) => string }) : { createHTML: (string) => string };
 
-    const button_playbackrate = document.createElement('button');
-    button_playbackrate.classList.add('_live_catch_up_playbackrate', 'ytp-button');
-    button_playbackrate.style.display = 'none';
-    button_playbackrate.style.cursor = 'default';
-    button_playbackrate.style.textAlign = 'center';
+    const button_playbackrate = create_elem('button', ['_live_catch_up_playbackrate', 'ytp-button']);
 
-    const button_latency = document.createElement('button');
-    button_latency.classList.add('_live_catch_up_latency', 'ytp-button');
-    button_latency.style.display = 'none';
-    button_latency.style.cursor = 'default';
-    button_latency.style.textAlign = 'center';
-    button_latency.style.fill = '#eee';
+    const button_latency = create_elem('button', ['_live_catch_up_latency', 'ytp-button']);
 
-    const button_health = document.createElement('button');
-    button_health.classList.add('_live_catch_up_health', 'ytp-button');
-    button_health.style.display = 'none';
-    button_health.style.cursor = 'default';
-    button_health.style.textAlign = 'center';
-    button_health.style.fill = '#eee';
+    const button_health = create_elem('button', ['_live_catch_up_health', 'ytp-button']);
 
-    const button_estimation = document.createElement('button');
-    button_estimation.classList.add('_live_catch_up_estimation', 'ytp-button');
-    button_estimation.style.display = 'none';
-    button_estimation.style.cursor = 'default';
-    button_estimation.style.width = 'auto';
-    button_estimation.style.fill = '#eee';
+    const button_estimation = create_elem('button', ['_live_catch_up_estimation', 'ytp-button']);
 
-    const msg_current = document.createElement('div');
-    msg_current.textContent = 'Copied!';
-    msg_current.style.display = 'none';
+    const msg_current = create_elem('button', ['_live_catch_up_msg_current', 'ytp-button']);
+    msg_current.innerHTML = HTMLPolicy.createHTML(`<span class="ytp-live">Copied!</span>`);
     msg_current.style.position = 'fixed';
 
-    const button_current = document.createElement('button');
-    button_current.classList.add('_live_catch_up_estimation', 'ytp-button');
-    button_current.style.display = 'none';
-    button_current.style.cursor = 'default';
-    button_current.style.width = 'auto';
-    button_current.style.fill = '#eee';
+    const button_current = create_elem('button', ['_live_catch_up_estimation', 'ytp-button']);
     button_current.addEventListener('click', () => {
         navigator.clipboard.writeText(button_current.getAttribute('current'));
 
-        const rect = button_current.getBoundingClientRect();
-        msg_current.style.left = `${rect.left + rect.width / 2.0}px`;
-        msg_current.style.top = `${rect.top - 16}px`;
-        msg_current.style.display = 'block';
-        const timeout_id = setTimeout(() => {
-            if (msg_current_timeout === timeout_id) {
-                msg_current.style.display = 'none';
-            }
+        if (new_style) {
+            msg_current.style.translate = '-32px -16px';
+        } else {
+            const rect = button_current.getBoundingClientRect();
+            msg_current.style.left = `${rect.left + rect.width / 2.0}px`;
+            msg_current.style.top = `${rect.top - 16}px`;
+        }
+
+        msg_current.style.display = 'inline-block';
+
+        clearTimeout(msg_current_timeout);
+        msg_current_timeout = setTimeout(() => {
+            msg_current.style.display = 'none';
         }, 4000);
-        msg_current_timeout = timeout_id;
     });
 
     const app = document.querySelector('ytd-app') ?? document.body; // YouTube.com or Embedded Player
 
     let player;
     let video;
-    let badge;
     let new_style;
     let interval;
     let interval_count = 0;
@@ -426,29 +387,25 @@
             return;
         }
 
-        badge = player.querySelector('button.ytp-live-badge');
-        if (!badge) {
-            return;
+        let area = player.querySelector('div.ytp-time-contents:has(button.ytp-live-badge)'); // new style
+        if (!area) {
+            area = player.querySelector('div.ytp-left-controls:has(button.ytp-live-badge)'); // old style
+            if (!area) {
+                return;
+            } else {
+                new_style = false;
+            }
+        } else {
+            new_style = true;
         }
 
         clearInterval(detect_interval);
 
         video.addEventListener('ratechange', onPlaybackRateChange);
 
-        if (player.querySelector('div.ytp-right-controls-left')) {
-            new_style = true;
-        } else {
-            new_style = false;
-        }
-
-        const area = new_style ? badge.parentElement.parentElement.parentElement : badge.parentElement.parentElement;
         let prev = undefined;
         for (const elem of [button_playbackrate, button_latency, button_health, button_current, msg_current, button_estimation].reverse()) {
-            if (prev) {
-                area.insertBefore(elem, prev);
-            } else {
-                area.appendChild(elem);
-            }
+            area.insertBefore(elem, prev);
             prev = elem;
         }
 
